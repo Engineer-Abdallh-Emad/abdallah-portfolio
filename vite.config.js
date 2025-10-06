dfaa8d89980ab54928f84fd7722f5001344189c7
Engineer-Abdallh-Emad <47742620+Engineer-Abdallh-Emad@users.noreply.github.com>
2025-10-06 11:45:25 +0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
   base: '/abdallah-portfolio/',
   resolve: {
     alias: {
-      "@": path.resolve("./src"),
+      "@": fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
